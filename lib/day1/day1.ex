2b6ca978e2da7775b7b5daddfd7110ce037a4f7a
Andrea Orler <andrea.orler@gmail.com>
2021-12-01 10:01:19 +0100

    feat: day1-part2

--- lib/day1/day1.ex
+++ lib/day1/day1.ex
@@ -10,6 +10,9 @@ defmodule V2021.Day1 do
 
   def solution_part2() do
     @input_file_part2
+    |> parse_input()
+    |> count_increase_depth_window(0)
+    |> IO.puts()
   end
 
   def parse_input(input) do
@@ -24,6 +27,13 @@ defmodule V2021.Day1 do
     new_count = maybe_increase_count(count, next > current)
 
     count_increase_depth([next] ++ rest, new_count)
+  end
+
+  def count_increase_depth_window([_, _, _ | []], count), do: count
+  def count_increase_depth_window([a, b, c, d | rest], count) do
+    new_count = maybe_increase_count(count, b + c + d > a + b + c)
+
+    count_increase_depth_window([b, c, d] ++ rest, new_count)
   end
 
   def maybe_increase_count(count, condition) do
