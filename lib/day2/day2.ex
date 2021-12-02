0705aa2c78ea648d746c30c485b9adaa2854536d
Andrea Orler <andrea.orler@gmail.com>
2021-12-02 09:35:12 +0100

    feat: day2-part2

--- lib/day2/day2.ex
+++ lib/day2/day2.ex
@@ -4,7 +4,7 @@ defmodule V2021.Day2 do
   def solution_part1() do
     @input_file_part1
     |> parse_input()
-    |> execute_plan()
+    |> execute_plan_part_1()
     |> Tuple.product()
     |> IO.inspect()
   end
@@ -12,9 +12,12 @@ defmodule V2021.Day2 do
   def solution_part2() do
     @input_file_part2
     |> parse_input()
+    |> execute_plan_part_2()
+    |> multiply()
     |> IO.inspect()
   end
 
+  # INPUT PARSING
   def parse_input(input) do
     input
     |> File.read!()
@@ -27,15 +30,19 @@ defmodule V2021.Day2 do
     %{ direction: direction, unit: String.to_integer(step) }
   end
 
-  def execute_plan(plan) do
-    Enum.reduce(plan, {0, 0}, &execute_step/2)
-  end
+  # PART 1
+  def execute_plan_part_1(plan), do: Enum.reduce(plan, {0, 0}, &execute_step_part_1/2)
 
-  def execute_step(step, state) do
-    case step[:direction] do
-      "up" -> {elem(state, 0), elem(state, 1) - step[:unit]}
-      "down" -> {elem(state, 0), elem(state, 1) + step[:unit]}
-      "forward" -> {elem(state, 0) + step[:unit], elem(state, 1)}
-    end
-  end
+  def execute_step_part_1(%{direction: "up", unit: unit}, {x, y}), do: {x, y - unit}
+  def execute_step_part_1(%{direction: "down", unit: unit}, {x, y}), do: {x, y + unit}
+  def execute_step_part_1(%{direction: "forward", unit: unit}, {x, y}), do: {x + unit, y}
+
+  # PART 2
+  def execute_plan_part_2(plan), do: Enum.reduce(plan, {0, 0, 0}, &execute_step_part_2/2)
+
+  def execute_step_part_2(%{direction: "up", unit: unit}, {x, y, a}), do: {x, y, a - unit}
+  def execute_step_part_2(%{direction: "down", unit: unit}, {x, y, a}), do: {x, y, a + unit}
+  def execute_step_part_2(%{direction: "forward", unit: unit}, {x, y, a}), do: {x + unit, y + a * unit, a}
+
+  def multiply({x, y, _}), do: x * y
 end
