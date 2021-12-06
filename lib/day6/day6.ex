4078dd6566a4556e035769ea7ab27e3107570341
Andrea Orler <andrea.orler@gmail.com>
2021-12-06 11:01:19 +0100

    feat: day6-part2

--- lib/day6/day6.ex
+++ lib/day6/day6.ex
@@ -12,6 +12,7 @@ defmodule V2021.Day6 do
   def solution_part2() do
     @input_file_part2
     |> parse_input()
+    |> simulate_reproduction(256)
     |> IO.inspect()
   end
 
@@ -21,12 +22,15 @@ defmodule V2021.Day6 do
     |> File.read!()
     |> String.split(",")
     |> Enum.map(&String.to_integer/1)
+    |> Enum.reduce(%{}, fn age, acc -> Map.update(acc, age, 1, &(&1 + 1)) end)
   end
 
-  def simulate_reproduction(base_lanternfish, target_day) do
-    base_lanternfish
+  def simulate_reproduction(lanterfish_ages, target_day) do
+    lanterfish_ages
+    |> Map.keys()
     |> Enum.map(&reproduct(&1, target_day))
-    |> Enum.sum()
+    |> Enum.zip(Map.values(lanterfish_ages))
+    |> Enum.reduce(0, fn {kids, occurences}, acc -> acc + kids * occurences end)
   end
 
   def reproduct(day_start, target_day) when target_day - day_start <= 0, do: 1
