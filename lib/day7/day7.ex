831926785e2038a5375cbbe55ebfa0eb454a3a06
Andrea Orler <andrea.orler@gmail.com>
2021-12-07 08:55:59 +0100

    feat: day7-part2

--- lib/day7/day7.ex
+++ lib/day7/day7.ex
@@ -6,16 +6,17 @@ defmodule V2021.Day7 do
     @input_file_part1
     |> parse_input()
     |> minmax()
-    |> distances()
-    |> Enum.sort_by(& elem(&1, 1))
-    |> List.first()
-    |> elem(1)
+    |> simple_distances()
+    |> extract_least_expensive()
     |> IO.inspect()
   end
 
   def solution_part2() do
     @input_file_part2
     |> parse_input()
+    |> minmax()
+    |> summation_distances()
+    |> extract_least_expensive()
     |> IO.inspect()
   end
 
@@ -32,7 +33,7 @@ defmodule V2021.Day7 do
     {positions, Enum.min_max(positions)}
   end
 
-  def distances({positions, {min, max}}) do
+  def simple_distances({positions, {min, max}}) do
     min..max
     |> Enum.map(&({&1, distance_for(positions, &1)}))
   end
@@ -42,4 +43,28 @@ defmodule V2021.Day7 do
     |> Enum.map(&abs(Kernel.-(&1, target)))
     |> Enum.sum()
   end
+
+  def extract_least_expensive(distances) do
+    distances
+    |> Enum.sort_by(& elem(&1, 1))
+    |> List.first()
+    |> elem(1)
+  end
+
+  # PART 2
+  def summation_distances({positions, {min, max}}) do
+    min..max
+    |> Enum.map(&({&1, summation_distance_for(positions, &1)}))
+  end
+
+  def summation_distance_for(positions, target) do
+    positions
+    |> Enum.reduce(%{}, fn position, acc ->
+      Map.update(acc, abs(target-position), 1, &(&1+1))
+    end)
+    |> Enum.map(&(summation(elem(&1, 0)) * elem(&1, 1)))
+    |> Enum.sum()
+  end
+
+  def summation(n), do: Enum.sum(1..n)
 end
